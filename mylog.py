8e876f6aecc3e89ce69070d505c9c0a154974151
sean <lsnmail@yeah.net>
2018-03-02 16:06:39 +0800

    add level set.

--- mylog.py
+++ mylog.py
@@ -11,23 +11,29 @@ import sys
 
 
 class MyLog(object):
-    def __init__(self):
+    def __init__(self, logfile):
         self.user = getpass.getuser()
         self.logger = logging.getLogger(self.user)
         self.logger.setLevel(logging.DEBUG)
 
-        self.logFile = sys.argv[0][0:-3] + ".log"
+        if logfile:
+            self.logFile = logfile
+        else:
+            self.logFile = sys.argv[0][0:-3] + ".log"
         self.formatter = logging.Formatter("%(asctime)-12s %(levelname)-8s %(name)-10s %(message)-12s\r\n")
         self.logHand = logging.FileHandler(self.logFile, encoding="utf8")
         self.logHand.setFormatter(self.formatter)
-        self.logHand.setLevel(logging.DEBUG)
+        self.logHand.setLevel(logging.INFO)  # 设置写入门槛
 
         self.logHandSt = logging.StreamHandler()
         self.logHandSt.setFormatter(self.formatter)
-        self.logHand.setLevel(logging.DEBUG)
+        self.logHandSt.setLevel(logging.WARNING)
 
         self.logger.addHandler(self.logHand)
         self.logger.addHandler(self.logHandSt)
+
+    def set_level(self, l):
+        self.logHand.setLevel(l)
 
     def debug(self, msg):
         self.logger.debug(msg)
@@ -36,7 +42,7 @@ class MyLog(object):
         self.logger.info(msg)
 
     def warn(self, msg):
-        self.logger.warnning(msg)
+        self.logger.warning(msg)
 
     def error(self, msg):
         self.logger.error(msg)
@@ -46,9 +52,10 @@ class MyLog(object):
 
 
 if __name__ == "__main__":
-    log = MyLog()
+    log = MyLog("m.log")
+    log.set_level(logging.ERROR)
     log.debug("I'm debug 中文")
     log.info("I'm info")
-    log.warn("I'm warnning")
+    log.warn("I'm warning")
     log.error("I'm error 中文")
     log.critical("I'm critical 中文")
